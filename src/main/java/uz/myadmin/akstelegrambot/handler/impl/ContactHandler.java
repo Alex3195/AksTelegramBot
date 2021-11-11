6f3736cbd327e40f57210982433b5e26900098fb
alex <alex9954161@mail.ru>
2021-11-11 18:14:53 +0500

    Initial state

--- src/main/java/uz/myadmin/akstelegrambot/handler/impl/ContactHandler.java
+++ src/main/java/uz/myadmin/akstelegrambot/handler/impl/ContactHandler.java
@@ -8,6 +8,7 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import uz.myadmin.akstelegrambot.commands.Commands;
+import uz.myadmin.akstelegrambot.constant.Status;
 import uz.myadmin.akstelegrambot.handler.Handler;
 import uz.myadmin.akstelegrambot.model.BotUserModel;
 import uz.myadmin.akstelegrambot.service.TelegramService;
@@ -22,11 +23,19 @@ public class ContactHandler implements Handler<Message> {
     @Override
     public void handleMessage(Message message) throws TelegramApiException {
         BotUserModel model = usersSevice.saveUser(message);
-        if (model != null) {
+        if (model.getStatus() == Status.PASSIVE) {
             telegramService.executeMessage(SendMessage
                     .builder()
                     .chatId(message.getChatId().toString())
                     .text("you are registered for being active ask for admin")
+                    .replyToMessageId(message.getMessageId())
+                    .replyMarkup(Commands.getEmptyKeyboard())
+                    .build());
+        }else {
+            telegramService.executeMessage(SendMessage
+                    .builder()
+                    .chatId(message.getChatId().toString())
+                    .text("you are already registered and activated")
                     .replyToMessageId(message.getMessageId())
                     .replyMarkup(Commands.getEmptyKeyboard())
                     .build());
